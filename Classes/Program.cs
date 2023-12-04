e6c597043948d0485e211ab0c3f09b7a07c918ab
Nasuha Asri <NASAS@orsted.com>
2023-12-04 23:01:00 +0800

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce (string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}.", to, Name);
+        }
+
+        // create person object from string
+        public Person Parse (string str)
+        {
+            var person = new Person ();
+            person.Name = str;
+
+            return person;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            // doesnt make sense, so we should put static keyword on Parse method
+            // var p = person.Parse("John");
+            // should be -> var person = Person.Parse("John")
+
+            person.Name = "Nur";
+            person.Introduce("Irfan");
+        }
+    }
+}
