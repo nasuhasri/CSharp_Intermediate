77f52069b69f5e7d63113ca60c1f7ec94e75f057
Nasuha Asri <NASAS@orsted.com>
2023-12-05 00:13:48 +0800

    classes theory

---
+++ Classes/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        // generic class that takes parameter so its called generic parameter
+        // it represents list of object and the parameter (Order) specifies type of obj
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            // make sure that the list of object always initialized
+            Orders = new List<Order>();
+        }
+
+        // call constructor w/o params first Customer() then, execute the rest of code
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,36 +3,19 @@ using System;
 
 namespace Classes
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce (string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}.", to, Name);
-        }
-
-        // create person object from string
-        public Person Parse (string str)
-        {
-            var person = new Person ();
-            person.Name = str;
-
-            return person;
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
+            var customer = new Customer();
 
-            // doesnt make sense, so we should put static keyword on Parse method
-            // var p = person.Parse("John");
-            // should be -> var person = Person.Parse("John")
+            var order = new Order();
+            customer.Orders.Add(order);
 
-            person.Name = "Nur";
-            person.Introduce("Irfan");
+            // if we didnt add constructor class, C# will auto make it for use
+
+            Console.WriteLine(customer.Id); // default value by C# - 0
+            Console.WriteLine(customer.Name); // default value by C# - null
         }
     }
 }
