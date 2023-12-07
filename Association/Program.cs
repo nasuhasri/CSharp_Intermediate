60196c4ff63fc4bf0c9a1d2a5e3daaa339e448ad
Nasuha Asri <NASAS@orsted.com>
2023-12-07 14:43:44 +0800

    converting object reference to its base class reference

--- Association/Program.cs
+++ Association/Program.cs
@@ -23,6 +23,16 @@
             // constructor inheritance
             // base class always executed first; Vehicle -> Car
             var car = new Car("XYZ123");
+
+            // upcasting - both shape and text1 class pointing/references to the same object in memory
+            // converting object reference to its base class reference
+            Text1 text1 = new Text1();
+            Shape shape = text1;
+
+            text1.Width = 200;
+            shape.Width = 100;
+
+            System.Console.WriteLine(text1.Width); // return 100
         }
     }
 }
---
+++ Association/Text1.cs
@@ -0,0 +1,8 @@
+﻿namespace Association
+{
+    public class Text1 : Shape
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+    }
+}
