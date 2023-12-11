7f89d0357a0e761770fb9e28173360bc287195a4
Nasuha Asri <NASAS@orsted.com>
2023-12-11 16:54:09 +0800

    testability (unit tests) with interfaces

---
+++ Testability.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testability.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ørsted  A/S")]
+[assembly: AssemblyProduct("Testability.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Ørsted  A/S 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("451855a7-63a9-41f4-a988-123dbd0acc95")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
