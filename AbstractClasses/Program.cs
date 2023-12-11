9ff2d1ed6e3852df3b595314e9da931c9d91d6ec
Nasuha Asri <NASAS@orsted.com>
2023-12-11 11:59:36 +0800

    abstract class and method

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cannot instantiate shape bcs it is abstract class
+            //var shape = new Shape();
+
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+        }
+    }
+}
