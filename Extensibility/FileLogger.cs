89960bb15a73f732907762ca09324514e15abd0b
Nasuha Asri <NASAS@orsted.com>
2023-12-13 10:38:00 +0800

    extensibility of application

---
+++ Extensibility/FileLogger.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace Extensibility
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+
+        public FileLogger(string path)
+        {
+            _path = path;
+        }
+
+        public void LogError(string message)
+        {
+            /*
+             * streamWriter use file resource and file resource is not managed by Common Language Runtime (CLR)
+             * means we need to dispose the resource once we finish using that
+             */
+            //var streamWriter = new StreamWriter(_path, true);
+            //streamWriter.WriteLine(message);
+
+            /*
+             * there is exception handling mechanism inside this code
+             * if something goes wrong ie exception goes wrong, compiler will make sure to close the file handle by calling dispose method on the streamWriter
+             * when we use using block, the compiler auto includes call to dispose()
+             */
+
+            Log(message, "ERROR");
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "INFO");
+        }
+
+        private void Log(string message, string messageType)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+            }
+        }
+    }
+}
---
+++ Extensibility/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // from DBMigrator class, we can create ConsoleLogger and FileLogger (extends the application)
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\Users\\nasas\\source\\repos\\Classes\\log.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
